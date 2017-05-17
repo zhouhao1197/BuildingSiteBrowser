3bf688639dbce7146f1f6815047291f0d01520ee
XueJia.Wang <wxjwangxuejia@outlook.com>
2017-05-17 21:46:57 +0800

    feat: Add Mouse View Controller
    Signed-off-by: XueJia.Wang <wxjwangxuejia@outlook.com>

---
+++ Assets/Scipts/CameraViewController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewController:Singleton<CameraViewController>
+{
+    public float RotateSensitive=300.0f;
+
+    [HideInInspector]
+    public Vector3 RotationOriginPosition;
+
+    private ComRef<Transform> mainCamera;
+
+    private Vector3 LastMousePosition;
+
+	private void Start()
+    {
+        mainCamera = new ComRef<Transform>(() =>
+         {
+             return Camera.main.transform;
+         });
+
+        RotationOriginPosition = mainCamera.Ref.position;
+        LastMousePosition = Input.mousePosition;
+	}
+	
+	private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            LastMousePosition = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(1))
+        {
+            DetectMouseAxis();
+        }
+	}
+
+    private void DetectMouseAxis()
+    {
+        Vector3 MouseDelta = Input.mousePosition - LastMousePosition;
+        LastMousePosition = Input.mousePosition;
+        mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(0, 1, 0), MouseDelta.x * RotateSensitive * Time.deltaTime);
+        mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(1, 0, 0), -MouseDelta.y * RotateSensitive * Time.deltaTime);
+    }
+}
---
+++ Assets/Scipts/ComRef.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComRef<T>
+{
+    public delegate T GetRefDelegate();
+    GetRefDelegate _getRef;
+
+    private T _ref;
+
+    public ComRef(GetRefDelegate getRef)
+    {
+        _getRef = getRef;
+    }
+
+    public T Ref
+    {
+        get
+        {
+            if (_ref == null)
+            {
+                if (_getRef != null)
+                {
+                    _ref = _getRef();
+                }
+            }
+            return _ref;
+        }
+    }
+}
