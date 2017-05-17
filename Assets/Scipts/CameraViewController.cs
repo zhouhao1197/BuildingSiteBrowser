a2ce7051f5b5049d5466d71efd6c58bab06ff582
XueJia.Wang <wxjwangxuejia@outlook.com>
2017-05-17 23:07:49 +0800

    feat: Add translate and resize(scroll) function
    Signed-off-by: XueJia.Wang <wxjwangxuejia@outlook.com>

--- Assets/Scipts/CameraViewController.cs
+++ Assets/Scipts/CameraViewController.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class CameraViewController:Singleton<CameraViewController>
 {
-    public float RotateSensitive=300.0f;
+    private enum CameraAction
+    {
+        rotate,
+        translate
+    }
 
-    /// <summary>
-    /// The Vector which is the delta position of camera and target obj
-    /// </summary>
+    [Tooltip("The Vector which is the delta position of camera and target obj")]
     public Vector3 MoveDistance = new Vector3(0, 10, -20);
+
+    public float RotateSensitive = 300.0f;
+    public float ScrollSensitive = 100.0f;
+    public float TranslateSensitive = 10.0f;
 
     [HideInInspector]
     public Vector3 RotationOriginPosition;
@@ -33,22 +39,41 @@ public class CameraViewController:Singleton<CameraViewController>
 	
 	private void Update()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)|| Input.GetMouseButtonDown(0))// To avoid hop
         {
             LastMousePosition = Input.mousePosition;
         }
         if (Input.GetMouseButton(1))
         {
-            DetectMouseAxis();
+            DetectMouseAxis(CameraAction.rotate);
         }
-	}
 
-    private void DetectMouseAxis()
+        if (Input.GetMouseButton(0))
+        {
+            DetectMouseAxis(CameraAction.translate);
+        }
+
+        float scrolldelta = Input.mouseScrollDelta.y;
+        if (scrolldelta != 0)
+        {
+            mainCamera.Ref.Translate(mainCamera.Ref.forward*scrolldelta*Time.deltaTime*ScrollSensitive);
+        }
+
+    }
+
+    private void DetectMouseAxis(CameraAction action)
     {
         Vector3 MouseDelta = Input.mousePosition - LastMousePosition;
         LastMousePosition = Input.mousePosition;
-        mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(0, 1, 0), MouseDelta.x * RotateSensitive * Time.deltaTime);
-        mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(1, 0, 0), -MouseDelta.y * RotateSensitive * Time.deltaTime);
+        if (action == CameraAction.rotate)
+        {
+            mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(0, 1, 0), MouseDelta.x * RotateSensitive * Time.deltaTime);
+            mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(1, 0, 0), -MouseDelta.y * RotateSensitive * Time.deltaTime);
+        }
+        else if(action==CameraAction.translate)
+        {
+            mainCamera.Ref.Translate(-MouseDelta* TranslateSensitive*Time.deltaTime);
+        }
     }
 
     public void SetCameraPosition(Vector3 TargetObjPosition)
