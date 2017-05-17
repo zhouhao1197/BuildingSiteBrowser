b35b8f7da74fbfb8bbe9d3c04bb68bb02ad52e05
XueJia.Wang <wxjwangxuejia@outlook.com>
2017-05-17 22:35:51 +0800

    feat:Add set target position UI
    1.when we set the target position,the camera will be away fomr obj in a direction and face the target
    2.have not set the Init position UI
    3.have not make the camera animation
    
    Signed-off-by: XueJia.Wang <wxjwangxuejia@outlook.com>

--- Assets/Scipts/CameraViewController.cs
+++ Assets/Scipts/CameraViewController.cs
@@ -6,14 +6,20 @@ public class CameraViewController:Singleton<CameraViewController>
 {
     public float RotateSensitive=300.0f;
 
+    /// <summary>
+    /// The Vector which is the delta position of camera and target obj
+    /// </summary>
+    public Vector3 MoveDistance = new Vector3(0, 10, -20);
+
     [HideInInspector]
     public Vector3 RotationOriginPosition;
 
     private ComRef<Transform> mainCamera;
-
     private Vector3 LastMousePosition;
+    private Quaternion DefaultRotation;
 
-	private void Start()
+
+    private void Start()
     {
         mainCamera = new ComRef<Transform>(() =>
          {
@@ -22,7 +28,8 @@ public class CameraViewController:Singleton<CameraViewController>
 
         RotationOriginPosition = mainCamera.Ref.position;
         LastMousePosition = Input.mousePosition;
-	}
+        DefaultRotation = Camera.main.transform.rotation;
+    }
 	
 	private void Update()
     {
@@ -42,5 +49,12 @@ public class CameraViewController:Singleton<CameraViewController>
         LastMousePosition = Input.mousePosition;
         mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(0, 1, 0), MouseDelta.x * RotateSensitive * Time.deltaTime);
         mainCamera.Ref.RotateAround(RotationOriginPosition, new Vector3(1, 0, 0), -MouseDelta.y * RotateSensitive * Time.deltaTime);
+    }
+
+    public void SetCameraPosition(Vector3 TargetObjPosition)
+    {
+        mainCamera.Ref.position = TargetObjPosition + MoveDistance;
+        RotationOriginPosition = TargetObjPosition;
+        mainCamera.Ref.rotation = Quaternion.LookRotation(-MoveDistance) * DefaultRotation;
     }
 }
---
+++ Assets/Scipts/UIManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singleton<UIManager>
+{
+
+    private Transform building;
+
+    private void Start()
+    {
+        building = GameObject.Find("Building").transform;
+    }
+
+    public void OnClickTitle(GameObject button)
+    {
+        if (button.transform.Find("SubTitle"))
+        {
+            GameObject _obj = button.transform.Find("SubTitle").gameObject;
+            bool subTitleActive = _obj.activeSelf;
+            _obj.SetActive(!subTitleActive);
+        }
+    }
+
+    public void OnClickPlaceRoot(string PlaceName)
+    {
+        Vector3 _placePosition = building.transform.Find(PlaceName).position;
+        CameraViewController.Instance.SetCameraPosition(_placePosition);
+    }
+}
